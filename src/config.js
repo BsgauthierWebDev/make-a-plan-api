e0da3e5177bbc95c04a18ce80ad6223aafba39c2
Brent Gauthier <brentsgauthier@gmail.com>
2021-04-04 10:38:47 -0400

    Fixed DATABASE_URL

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 module.exports = {
     PORT: process.env.PORT || 8000,
     NODE_ENV: process.env.NODE_ENV || 'development',
-    DATABASE_URL: process.env.DATABASE_URL || 'https://https://pacific-hamlet-55041.herokuapp.com/',
+    DATABASE_URL: process.env.DATABASE_URL || 'https://pacific-hamlet-55041.herokuapp.com/',
     TEST_DATABASE_URL: 'postgresql://dunder_mifflin@localhost/make-a-plan-test'
 }
