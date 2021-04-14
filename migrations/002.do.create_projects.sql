4e98b479dbd71961cd4217c46a174cfe3289c6c1
Brent Gauthier <brentsgauthier@gmail.com>
2021-04-14 19:30:21 -0400

    Added new migrations

--- migrations/002.do.create_projects.sql
+++ migrations/002.do.create_projects.sql
@@ -9,24 +9,3 @@ CREATE TABLE projects (
     user_id INTEGER
         REFERENCES users(id) ON DELETE CASCADE NOT NULL
 );
-
-BEGIN;
-
-TRUNCATE
-    users,
-    projects
-    RESTART IDENTITY CASCADE;
-
-INSERT INTO users (email, username, password)
-VALUES
-    ('test123@test.com', 'username123', 'password123'),
-    ('test456@test.com', 'username456', 'password456'),
-    ('test789@test.com', 'username789', 'password789');
-
-INSERT INTO projects (name, description, materials, steps, user_id)
-VALUES  
-    ('Test 1', '1st test project', 'Test materials 1', 'Test steps 1', 1),
-    ('Test 2', '2nd test project', 'Test Materials 2', 'Test steps 2', 2),
-    ('Test 3', null, 'Test materials 3', 'Test steps 3', 3);
-
-COMMIT;
