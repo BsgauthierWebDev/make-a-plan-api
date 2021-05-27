6b1b272d1bbc41d95160093790d4ad0a9d7356ca
Brent Gauthier <brentsgauthier@gmail.com>
2021-05-27 11:26:32 -0400

    Moving project to production

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 module.exports = {
     PORT: process.env.PORT || 8000,
-    NODE_ENV: process.env.NODE_ENV || 'development',
+    NODE_ENV: process.env.NODE_ENV || 'production',
     DATABASE_URL: process.env.DATABASE_URL || 'https://pacific-hamlet-55041.herokuapp.com/',
     TEST_DATABASE_URL: 'postgresql://dunder_mifflin@localhost/make-a-plan-test',
     JWT_SECRET: process.env.JWT_SECRET || 'change-this-secret',
--- src/server.js
+++ src/server.js
@@ -6,7 +6,9 @@ const {PORT, DATABASE_URL} = require('./config')
 
 const db = knex({
     client: 'pg',
-    connection: DATABASE_URL
+    connection: (process.env.NODE_ENV === 'test')
+    ? TEST_DATABASE_URL
+    : DATABASE_URL,
 })
 
 app.set('db', db)
